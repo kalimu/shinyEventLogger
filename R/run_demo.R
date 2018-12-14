4682f3df2e10d2f22d62fc71008a997654d7506b
Kamil Wais <kamil.wais@contractors.roche.com>
2018-12-14 08:26:31 +0100

    adding running demo app in background

--- R/run_demo.R
+++ R/run_demo.R
@@ -1,10 +1,21 @@
 #' @export
 
-run_demo <- function() {
+run_demo <- function(in_background = FALSE) {
 
   app_path <- system.file("shiny", "demoapp", package = "shinyEventLogger")
 
-  shiny::runApp(app_path, display.mode = "normal")
+  if (in_background) {
+
+    system("R -e \"shiny::runApp('inst/shiny/demoapp', port = 5555)\"",
+           wait = FALSE)
+
+    message("The App is available on 127.0.0.1:5555")
+
+  } else {
+
+    shiny::runApp(app_path, display.mode = "normal")
+
+  }
 
 } # end of run_demo
 
