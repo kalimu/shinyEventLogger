86b81758207720b47b46006bcaa788b3fc06ce6a
Kamil Wais <kamil.wais@contractors.roche.com>
2019-01-14 07:45:02 +0100

    adding debug_objects

---
+++ R/debug_object.R
@@ -0,0 +1,61 @@
+#' Copying objects to global environment
+#'
+#' With \code{debug_objects} you can copy an object to the global environment
+#' for further debugging or developing.
+#'
+#' @param ... Named objects to be copy to the global environment.
+#' If there is only one unnamed object, its name in the global environment
+#' will be the same as the name passed to \code{...}.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' if (interactive()) {
+#'
+#'   shiny::shinyApp(
+#'     ui = shiny::fluidPage(log_init()),
+#'     server = function(input, output) {
+#'       set_logging()
+#'       debug_objects(mtcars)
+#'       debug_objects(df1 = head(mtcars), df2 = head(iris))
+#'     }
+#'   )
+#'
+#' }
+#' }
+
+debug_objects <- function(...) {
+
+  session <- shiny::getDefaultReactiveDomain()
+  input <- session$input
+
+  objects <- eval(list(...))
+  object_names <- names(objects)
+
+  if (NROW(objects) == 1 && is.null(object_names))  {
+
+    object_names <- deparse(substitute(...))
+
+  }
+
+  if (NROW(objects) > 1 &&
+      (is.null(object_names) || any(object_names == ""))
+      ) {
+
+    stop("All objects must be named.")
+
+  }
+
+  walk2(objects, object_names,
+        function(object, object_name) {
+
+          assign(object_name, value = object, envir = .GlobalEnv)
+
+          message("Object named `", object_name,
+                  "` was assigned to the global environment for debugging.\n")
+
+  }) # end of walk2
+
+} # end of debug_objects()
